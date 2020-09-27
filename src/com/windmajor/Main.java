4274121d74a5e5aa1ed181438c74d8c64dc13cc0
WangMeng <wangmeng3965@gmail.com>
2020-09-27 14:53:28 +0800

    增加题目771宝石与石头的3种解决方案【已复查】

--- src/com/windmajor/Main.java
+++ src/com/windmajor/Main.java
@@ -1,13 +1,17 @@
 package com.windmajor;
 
 import com.windmajor.question.Solution_001;
+import com.windmajor.question.Solution_701;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int[] result = new Solution_001().twoSum2(new int[]{2, 7, 11, 15}, 9);
-        System.out.println("result[0] = " + result[0]);
-        System.out.println("result[1] = " + result[1]);
+//        int[] result = new Solution_001().twoSum2(new int[]{2, 7, 11, 15}, 9);
+//        System.out.println("result[0] = " + result[0]);
+//        System.out.println("result[1] = " + result[1]);
+
+        int result = new Solution_701().numJewelsInStones("z", "ZZ");
+        System.out.println(result);
     }
 }
---
+++ src/com/windmajor/question/Solution_701.java
@@ -0,0 +1,46 @@
+package com.windmajor.question;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Solution_701 {
+
+    /* 771. 宝石与石头 */
+    public int numJewelsInStones(String J, String S) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char sChar : S.toCharArray()) {
+            map.put(sChar, map.getOrDefault(sChar, 0) + 1);
+        }
+        int result = 0;
+        for (char jChar : J.toCharArray()) {
+            result += map.getOrDefault(jChar, 0);
+        }
+        return result;
+    }
+
+    public int numJewelsInStones2(String J, String S) {
+        HashSet<Character> set = new HashSet<>();
+        for (char jChar : J.toCharArray()) {
+            set.add(jChar);
+        }
+        int result = 0;
+        for (char sChar : S.toCharArray()) {
+            if (set.contains(sChar)) {
+                result += 1;
+            }
+        }
+        return result;
+    }
+
+    public int numJewelsInStones3(String J, String S) {
+        int result = 0;
+        for (char jChar : J.toCharArray()) {
+            for (char sChar : S.toCharArray()) {
+                if (jChar == sChar) {
+                    result += 1;
+                }
+            }
+        }
+        return result;
+    }
+}
