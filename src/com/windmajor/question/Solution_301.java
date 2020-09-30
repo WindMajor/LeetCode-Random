0a9083012ddc468abd1a36c6cfeb282c37f84db1
WangMeng <wangmeng3965@gmail.com>
2020-09-30 16:09:49 +0800

    增加题目380. 常数时间插入、删除和获取随机元素的解答【已复查】

--- src/com/windmajor/question/Solution_301.java
+++ src/com/windmajor/question/Solution_301.java
@@ -95,6 +95,56 @@ public class Solution_301 {
         return Arrays.copyOf(result, k);
     }
 
+    /* 380. 常数时间插入、删除和获取随机元素 */
+    public static class RandomizedSet {
+        Map<Integer, Integer> dict;
+        List<Integer> list;
+        Random rand = new Random();
+
+        /**
+         * Initialize your data structure here.
+         */
+        public RandomizedSet() {
+            dict = new HashMap<>();
+            list = new ArrayList<>();
+        }
+
+        /**
+         * Inserts a value to the set. Returns true if the set did not already contain the specified element.
+         */
+        public boolean insert(int val) {
+            if (dict.containsKey(val)) return false;
+
+            list.add(list.size(), val);
+            dict.put(val, list.size() - 1);
+            return true;
+        }
+
+        /**
+         * Removes a value from the set. Returns true if the set contained the specified element.
+         */
+        public boolean remove(int val) {
+            if (!dict.containsKey(val)) return false;
+
+            int removeIndex = dict.get(val);
+            int lastElement = list.get(list.size() - 1);
+
+            list.set(removeIndex, lastElement);
+            dict.put(lastElement, removeIndex);
+
+            list.remove(list.size() - 1);
+            dict.remove(val);
+            return true;
+        }
+
+        /**
+         * Get a random element from the set.
+         */
+        public int getRandom() {
+            return list.get(rand.nextInt(list.size()));
+        }
+    }
+
     /* 387. 字符串中的第一个唯一字符 */
     public int firstUniqChar(String s) {
         HashMap<Character, Integer> map = new HashMap<>();
