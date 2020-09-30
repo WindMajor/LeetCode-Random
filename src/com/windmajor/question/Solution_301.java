dcdc4ac54c551fe0afddec09601680b35e9acb83
WangMeng <wangmeng3965@gmail.com>
2020-09-30 11:48:40 +0800

    增加题目347. 前 K 个高频元素的2种解答【已复查】

--- src/com/windmajor/question/Solution_301.java
+++ src/com/windmajor/question/Solution_301.java
@@ -1,10 +1,78 @@
 package com.windmajor.question;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.*;
 
 public class Solution_301 {
+
+    /* 347. 前 K 个高频元素 */
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int n : nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+        PriorityQueue<int[]> queue = new PriorityQueue<>(Comparator.comparingInt(x -> x[1]));
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int num = entry.getKey();
+            int count = entry.getValue();
+
+            if (queue.size() == k) {
+                if (queue.peek()[1] < count) {
+                    queue.poll();
+                    queue.offer(new int[]{num, count});
+                }
+            } else {
+                queue.offer(new int[]{num, count});
+            }
+        }
+
+        int[] ans = new int[k];
+        for (int i = 0; i < k; i++) {
+            ans[i] = queue.poll()[0];
+        }
+        return ans;
+    }
+
+    public int[] topKFrequent2(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int n : nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+
+        List<int[]> list = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            list.add(new int[]{entry.getKey(), entry.getValue()});
+        }
+
+        int[] ans = new int[k];
+        quickSort(list, 0, list.size() - 1, ans, 0, k);
+        return ans;
+    }
+
+    public void quickSort(List<int[]> list, int start, int end, int[] ans, int ansIndex, int k) {
+        int pivot = list.get(start)[1];
+        int index = start;
+        for (int i = start + 1; i <= end; i++) {
+            if (list.get(i)[1] >= pivot) {
+                index += 1;
+                Collections.swap(list, index, i);
+            }
+        }
+        Collections.swap(list, index, start);
+
+        if (k <= index - start) {
+            quickSort(list, start, index - 1, ans, ansIndex, k);
+        } else {
+            for (int i = start; i <= index; i++) {
+                ans[ansIndex] = list.get(i)[0];
+                ansIndex += 1;
+            }
+            if (k > index - start + 1) {
+                quickSort(list, index + 1, end, ans, ansIndex, k - (index - start + 1));
+            }
+        }
+    }
+
 
     /* 350. 两个数组的交集 II */
     public int[] intersect(int[] nums1, int[] nums2) {
