7e1cf3817feaa8c166dbc69aa77dfb027e0214f2
WangMeng <wangmeng3965@gmail.com>
2020-10-01 12:31:39 +0800

    增加题目88. 合并两个有序数组的3种解答方法【已复查】

--- src/com/windmajor/question/Solution_001.java
+++ src/com/windmajor/question/Solution_001.java
@@ -123,4 +123,41 @@ public class Solution_001 {
         }
         return new ArrayList<>(map.values());
     }
+
+    /* 88. 合并两个有序数组 */
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        System.arraycopy(nums2, 0, nums1, m, n);
+        Arrays.sort(nums1);
+    }
+
+    public void merge2(int[] nums1, int m, int[] nums2, int n) {
+        int[] nums1_copy = new int[m];
+        System.arraycopy(nums1, 0, nums1_copy, 0, m);
+
+        int p1 = 0;
+        int p2 = 0;
+
+        int p = 0;
+
+        while ((p1 < m) && (p2 < n)) {
+            nums1[p++] = (nums1_copy[p1] < nums2[p2]) ? nums1_copy[p1++] : nums2[p2++];
+        }
+
+        if (p1 < m)
+            System.arraycopy(nums1_copy, p1, nums1, p1 + p2, m + n - p1 - p2);
+        if (p2 < n)
+            System.arraycopy(nums2, p2, nums1, p1 + p2, m + n - p1 - p2);
+    }
+
+    public void merge3(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int p = m + n - 1;
+
+        while ((p1 >= 0) && (p2 >= 0)) {
+            nums1[p--] = (nums1[p1] < nums2[p2]) ? nums2[p2--] : nums1[p1--];
+        }
+
+        System.arraycopy(nums2, 0, nums2, 0, p2 + 1);
+    }
 }
