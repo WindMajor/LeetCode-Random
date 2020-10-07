39abef7e1bc334ed21f69eaff2d691445faa39b7
WangMeng <wangmeng3965@gmail.com>
2020-10-07 08:14:06 +0800

    添加题目278. 第一个错误的版本的解答；主要是用到了两分法的策略【已复查】

--- src/com/windmajor/question/Solution_201.java
+++ src/com/windmajor/question/Solution_201.java
@@ -47,4 +47,23 @@ public class Solution_201 {
         }
         return false;
     }
+
+    /* 278. 第一个错误的版本 */
+    private boolean isBadVersion(int version) {
+        return true;
+    }
+
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
 }
