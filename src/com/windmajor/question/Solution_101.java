280cadd991778899a49b39c1a49239ea1fe3d623
WangMeng <wangmeng3965@gmail.com>
2020-10-10 19:50:18 +0800

    添加题目198. 打家劫舍的2种解决方案【已复查】

--- src/com/windmajor/question/Solution_101.java
+++ src/com/windmajor/question/Solution_101.java
@@ -70,4 +70,54 @@ public class Solution_101 {
         }
         return ans;
     }
+
+    /* 198. 打家劫舍 */
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int length = nums.length;
+        if (length == 1) {
+            return nums[0];
+        }
+
+        if (length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+
+        int[] dp = new int[length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < length; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[length - 1];
+    }
+
+    public int rob2(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int length = nums.length;
+        if (length == 1) {
+            return nums[0];
+        }
+
+        if (length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+
+        int first = nums[0];
+        int second = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < length; i++) {
+            int temp = second;
+            second = Math.max(first + nums[i], second);
+            first = temp;
+        }
+        return second;
+    }
 }
