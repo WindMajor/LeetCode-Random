3d2c09cdc25cf368aaef309edd954dcaa772152d
WangMeng <wangmeng3965@gmail.com>
2020-10-13 11:02:20 +0800

    增加题目13.罗马数字转整数的解法【已复查】

--- src/com/windmajor/question/Solution_001.java
+++ src/com/windmajor/question/Solution_001.java
@@ -77,6 +77,36 @@ public class Solution_001 {
         return result;
     }
 
+    /* 13. 罗马数字转整数 */
+    public int romanToInt(String s) {
+        int ans = 0;
+        int preNum = getValue(s.charAt(0));
+        for (int i = 1; i < s.length(); i++) {
+            int num = getValue(s.charAt(i));
+            if (preNum < num) {
+                ans -= preNum;
+            } else {
+                ans += preNum;
+            }
+            preNum = num;
+        }
+        ans += preNum;
+        return ans;
+    }
+
+    private int getValue(char ch) {
+        switch (ch) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+
 
     /* 36. 有效的数独 */
     public boolean isValidSudoku(char[][] board) {
