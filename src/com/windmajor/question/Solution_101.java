56f18060585f13c1de907a4c65911a683c46c8f3
WangMeng <wangmeng3965@gmail.com>
2020-10-17 22:21:19 +0800

    增加题目190. 颠倒二进制位的3种解答方法【已复查】

--- src/com/windmajor/question/Solution_101.java
+++ src/com/windmajor/question/Solution_101.java
@@ -102,6 +102,33 @@ public class Solution_101 {
         }
     }
 
+    /* 190. 颠倒二进制位 */
+    public int reverseBits(int n) {
+        int ans = 0;
+        for (int i = 0; i < 32; i++) {
+            ans = (ans << 1) + (n & 1);
+            n >>= 1;
+        }
+        return ans;
+    }
+
+    public int reverseBits2(int n) {
+        int ans = 0;
+        for (int i = 0; i < 32; i++) {
+            ans ^= ((i << i) & n) != 0 ? (1 << (31 - i)) : 0;
+        }
+        return ans;
+    }
+
+    public int reverseBits3(int n) {
+        n = (n >>> 16) | (n << 16);
+        n = ((n & 0xff00ff00) >>> 8) | ((n & 0x00ff00ff) << 8);
+        n = ((n & 0xf0f0f0f0) >>> 4) | ((n & 0x0f0f0f0f) << 4);
+        n = ((n & 0xcccccccc) >>> 2) | ((n & 0x33333333) << 2);
+        n = ((n & 0xaaaaaaaa) >>> 1) | ((n & 0x55555555) << 1);
+        return n;
+    }
+
     /* 191. 位1的个数 */
     public int hammingWeight(int n) {
         int bits = 0;
