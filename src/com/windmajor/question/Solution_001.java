38d49428d390309f7b6ab87bfb233560216bdc7f
WangMeng <wangmeng3965@gmail.com>
2020-10-23 14:09:46 +0800

    添加题目20. 有效的括号的解答【已复查】

--- src/com/windmajor/question/Solution_001.java
+++ src/com/windmajor/question/Solution_001.java
@@ -107,6 +107,34 @@ public class Solution_001 {
         }
     }
 
+    /* 20. 有效的括号 */
+    public boolean isValid(String s) {
+        int n = s.length();
+        if (n % 2 == 1) {
+            return false;
+        }
+
+        Map<Character, Character> pairs = new HashMap<>();
+        pairs.put(')', '(');
+        pairs.put(']', '[');
+        pairs.put('}', '{');
+
+        Deque<Character> stack = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (pairs.containsKey(ch)) {
+                if (stack.isEmpty() || stack.peek() != pairs.get(ch)) {
+                    return false;
+                }
+                stack.pop();
+
+            } else {
+                stack.push(ch);
+            }
+        }
+        return stack.isEmpty();
+    }
+
 
     /* 36. 有效的数独 */
     public boolean isValidSudoku(char[][] board) {
